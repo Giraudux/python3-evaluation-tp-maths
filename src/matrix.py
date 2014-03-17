1d65a80cf76501a5ac4bc99d1e35d9f1033e6146
Giraudux <giraudux@gmail.com>
2014-03-17 01:45:29 +0100

    fix getitem, setitem and mul functions; class Matrix -> matrix; add get_row and get column functions

--- src/matrix.py
+++ src/matrix.py
@@ -1,6 +1,11 @@
 from array import array
 
-class Matrix:
+"""
+ * Class matrix
+ * i, p = rows
+ * j, q = columns
+"""
+class matrix:
     def __init__(self, p, q, ls=None):
         self.p = p
         self.q = q
@@ -21,11 +26,11 @@ class Matrix:
 
     def __getitem__(self, key):
         i, j = key
-        return self.data[self.q*(i-1)+j-1]
+        return self.data[self.q*i+j]
 
     def __setitem__(self, key, value):
         i, j = key
-        self.data[self.q*(i-1)+j-1] = value
+        self.data[self.q*i+j] = value
 
     def __add__(self, other):
         if((self.q == other.q) or (self.p == other.p)):
@@ -49,9 +54,9 @@ class Matrix:
 
     def __mul__(self, other):
         if(self.q == other.p):
-            res = Matrix(self.p, other.q)
+            res = matrix(self.p, other.q)
             for i in range(self.p):
-                for j in range(self.q):
+                for j in range(other.q):
                     acc=0
                     for k in range(self.q):
                         acc += self[i,k] * other[k,j]
@@ -60,8 +65,14 @@ class Matrix:
         else:
             return None
 
-mat1 = Matrix(2,2,[1,2,3,4])
-mat2 = Matrix(2,2,[4,3,2,1])
-print(mat1)
-print(mat2)
-print(mat1 * mat2)
+    def get_row(self, i):
+        res = list()
+        for j in range(self.q):
+            res.append(self[i,j])
+        return res 
+
+    def get_column(self, j):
+        res = list()
+        for i in range(self.p):
+            res.append(self[i,j])
+        return res 
