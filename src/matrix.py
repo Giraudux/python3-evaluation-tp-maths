74edf6431b46c9b60b6cbcf807df941a291b5952
Giraudux <Giraudux@users.noreply.github.com>
2014-03-20 14:08:36 +0100

    set class variable to protected

--- src/matrix.py
+++ src/matrix.py
@@ -8,58 +8,58 @@ from array import array
 class matrix:
 
     def __init__(self, p, q, data=None):
-        self.p = p
-        self.q = q
+        self._p = p
+        self._q = q
         if isinstance(data, list):
             self.from_list(data)
         else:
-            self.data = array("d", [0]*(p*q))
+            self._data = array("d", [0]*(p*q))
 
     def __repr__(self):
-        return "matrix({self.p}, {self.q}, {self.data})".format(self=self)
+        return "matrix({self._p}, {self._q}, {self._data})".format(self=self)
 
     def __str__(self):
         res = str()
-        tmp = self.data.tolist()
-        for i in range(0,self.p*self.q,self.q):
-            res += str(tmp[i:i+self.q]) + "\n"
+        tmp = self._data.tolist()
+        for i in range(0,self._p*self._q,self._q):
+            res += str(tmp[i:i+self._q]) + "\n"
         return res
 
     def __getitem__(self, key):
         i, j = key
-        return self.data[self.q*i+j]
+        return self._data[self._q*i+j]
 
     def __setitem__(self, key, value):
         i, j = key
-        self.data[self.q*i+j] = value
+        self._data[self._q*i+j] = value
 
     def __add__(self, other):
-        if((self.q == other.q) or (self.p == other.p)):
-            res = Matrix(self.p, self.q)
-            for i in range(self.p):
-                for j in range(self.q):
+        if((self._q == other._q) or (self._p == other._p)):
+            res = Matrix(self._p, self._q)
+            for i in range(self._p):
+                for j in range(self._q):
                     res[i,j] = self[i,j] + other[i,j]
             return res
         else:
             return None
 
     def __sub__(self, other):
-        if((self.q == other.q) or (self.p == other.p)):
-            res = Matrix(self.p, self.q)
-            for i in range(self.p):
-                for j in range(self.q):
+        if((self._q == other._q) or (self._p == other._p)):
+            res = Matrix(self._p, self._q)
+            for i in range(self._p):
+                for j in range(self._q):
                     res[i,j] = self[i,j] - other[i,j]
             return res
         else:
             return None
 
     def __mul__(self, other):
-        if(self.q == other.p):
-            res = matrix(self.p, other.q)
-            for i in range(self.p):
-                for j in range(other.q):
+        if(self._q == other._p):
+            res = matrix(self._p, other._q)
+            for i in range(self._p):
+                for j in range(other._q):
                     acc=0
-                    for k in range(self.q):
+                    for k in range(self._q):
                         acc += self[i,k] * other[k,j]
                     res[i,j] = acc
             return res
@@ -67,27 +67,27 @@ class matrix:
             return None
 
     def __eq__(self, other):
-        return (self.p == other.p) and (self.q == other.q) and (self.data == other.data)
+        return (self._p == other._p) and (self._q == other._q) and (self._data == other._data)
 
     def __ne__(self, other):
         return not(self == other)
 
     def from_list(self, data):
-        if(len(data) == (self.p*self.q)):
-            self.data = array("d", data)
+        if(len(data) == (self._p*self._q)):
+            self._data = array("d", data)
 
     def from_json(self, json):
         return None
 
     def get_row(self, i):
         res = list()
-        for j in range(self.q):
+        for j in range(self._q):
             res.append(self[i,j])
         return res 
 
     def get_column(self, j):
         res = list()
-        for i in range(self.p):
+        for i in range(self._p):
             res.append(self[i,j])
         return res 
 
@@ -97,15 +97,19 @@ class matrix:
 class lu_decomposition:
 
     def __init__(self, a, in_place=True):
-        self.a = a
+        if not in_place:
+            self._a = a
+        else:
+            self._a = None
+        self._lu = a
         self.decompose()
 
     def decompose(self):
-        for k in range(self.a.p-1):
-            for i in range(k+1,self.a.p):
-                self.a[i,k] = self.a[i,k]/self.a[k,k]
-                for j in range(k+1,self.a.p):
-                    self.a[i,j] = self.a[i,j]-self.a[i,k]*self.a[k,j]
+        for k in range(self._lu.get_p()-1):
+            for i in range(k+1,self._lu.get_p()):
+                self._lu[i,k] = self._lu[i,k]/self._lu[k,k]
+                for j in range(k+1,self._lu.get_p()):
+                    self._lu[i,j] = self._lu[i,j]-self._lu[i,k]*self._lu[k,j]
 
     def get_a(self):
-        return self.a
+        return self._a
